b7efdda352047989bcf8f55e83dc8c2992d4c978
Gordon Freeman <drfr33man@protonmail.com>
2022-10-17 18:16:58 +0300

    working on auth

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
 const qrcode = require("qrcode-terminal");
-
+const express = require("express");
 const { Client, NoAuth, LocalAuth } = require("whatsapp-web.js");
+const axios = require("axios");
+
+const secret = "1234";
 const client = new Client({
   authStrategy: new LocalAuth(),
 });
+
+const app = express();
+
+app.use((req, res, next) => {
+  const { pass } = req.body;
+  if (pass !== secret) {
+    next(new Error({ status: 401, message: "Bad password" }));
+  }
+  next();
+});
+app.post("/sendMessage", function (req, res) {
+  const { phone, msg, pass } = req.body;
+  sendMessage(phone, msg);
+});
+
+app.listen(3000);
 
 client.on("qr", (qr) => {
   qrcode.generate(qr, { small: true });
